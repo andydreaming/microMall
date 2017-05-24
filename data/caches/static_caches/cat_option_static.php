12c9019f6c3dcb3e0ea4b70dd3d42cef99124089
shirui.zhang <shirui.zhang@56qq.com>
2017-05-24 18:52:23 +0800

    add code

---
+++ data/caches/static_caches/cat_option_static.php
@@ -0,0 +1,36 @@
+<?php
+$data = array (
+  1 => 
+  array (
+    'cat_id' => '1',
+    'cat_name' => '手机',
+    'measure_unit' => '',
+    'parent_id' => '0',
+    'is_show' => '1',
+    'show_in_nav' => '0',
+    'grade' => '0',
+    'sort_order' => '50',
+    'has_children' => '1',
+    'goods_num' => 0,
+    'level' => 0,
+    'id' => '1',
+    'name' => '手机',
+  ),
+  2 => 
+  array (
+    'cat_id' => '2',
+    'cat_name' => '华为',
+    'measure_unit' => '',
+    'parent_id' => '1',
+    'is_show' => '1',
+    'show_in_nav' => '0',
+    'grade' => '0',
+    'sort_order' => '50',
+    'has_children' => '0',
+    'goods_num' => '1',
+    'level' => 1,
+    'id' => '2',
+    'name' => '华为',
+  ),
+);
+?>
